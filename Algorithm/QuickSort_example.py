c8325fa3262e3768abccae6aacf221b4277532f0
junhyuk843 <junhyuk843@gmail.com>
2022-05-09 00:42:10 +0900

    commit

--- Algorithm/QuickSort_example.py
+++ Algorithm/QuickSort_example.py
@@ -1,20 +1,28 @@
+import sys
+sys.setrecursionlimit(10**6)
+#RecursionError 발생 
+#해결책: 파이썬이 제한하는 재귀함수의 깊이를 변경했음
+A=list()
 def quicksort(A,first,last):
-    p=A[first]
+    if first >= last:
+        return
+    p=first
     left = first+1
     right = last
 
     while left <= right :
-        while left <= last and A[left] < p :
+        while left <= last and A[left] <= A[p] :
             left+=1
-        while A[right] > p :
+        while right > first and A[right] >= A[p] :
             right-=1
-        if left <= right :
+
+        if left>right:
+            A[right],A[p] = A[p],A[right]
+        else:
             A[left],A[right] = A[right],A[left]
-            left+=1
-            right-=1
-        A[first],A[right] = A[right],A[first]
-        quicksort(A,first,right-1)
-        quicksort(A,left,last)
+
+    quicksort(A,first,right-1)
+    quicksort(A,right+1,last)
 
 A=[2,4,1,5,3,9]
 print(A)
@@ -23,8 +31,8 @@ print(A)
             
 
 
-
-
+#b.c O(NlogN)
+#w.c O(N^2)
 
 #quick sort algorithm
 #in-place quick sort algorithm
